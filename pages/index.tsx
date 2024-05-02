712ff8f74e05f27bb3a463a9f53d99c5546fe9e7
Thejas Kiran P S <thejaskiranps@gmail.com>
2024-05-02 21:13:56 +0530

    remove yarn

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
-import { Button, Center, Group, Stack } from "@mantine/core";
+import { Button, Center, Stack } from "@mantine/core";
 import { FileUploader } from "../components/FileUploader/FileUploader";
 
 export default function IndexPage() {
   return (
     <Center
-
       h={'100%'}
-
     >
       <Stack
         maw={900}
